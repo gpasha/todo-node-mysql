04fd15fbd4f53648c6c8d01843e3a58903a88ae8
gpasha <g1ebikpasha@gmail.com>
2021-07-30 18:16:38 +0300

    sequelize, mysql2, mysql com. ser., connect to db

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const path = require('path')
+const sequelize = require('./utils/database')
+const todosRouter = require('./routes/todos')
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public') ))
+app.use('/api/todos', todosRouter)
+
+
+
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-
 const PORT = process.env.PORT || 3000
-app.listen(PORT)
+
+async function start() {
+    try {
+        await sequelize.sync()
+        app.listen(PORT)
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+start()
